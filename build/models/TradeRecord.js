c714ce840bd5501d20e41ae05bcd69f2e30ed6be
Zack Ward <zack@zadiastudio.com>
2017-07-13 14:26:59 -0600

    Add TradeRecord model

---
+++ build/models/TradeRecord.js
@@ -0,0 +1,14 @@
+module.exports = function (sequelize, DataTypes) {
+    let TradeRecord = sequelize.define('tradeRequest', {}, {
+        classMethods: {
+            associate: function (models) {
+                console.log("Associating TradeRecord model.");
+                TradeRecord.belongsTo(models.User, { as: "tradeSender" });
+                TradeRecord.belongsTo(models.Book, { as: "tradeSenderBook" });
+                TradeRecord.belongsTo(models.User, { as: "tradeReceiver" });
+                TradeRecord.belongsTo(models.Book, { as: "tradeReceiverBook" });
+            }
+        }
+    });
+    return TradeRecord;
+};
