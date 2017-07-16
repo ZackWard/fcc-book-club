5d5d80ca86e8ae2b63b8e56f0e6ee6e3922449ae
Zack Ward <zack@zadiastudio.com>
2017-07-15 19:11:31 -0600

    Remove build directory from vcs

--- build/config/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv = require("dotenv");
-dotenv.config();
-exports.default = {
-    cookieSecret: process.env.APP_SECRET,
-    dbFileName: (process.env.NODE_ENV == "testing") ? "bookclub.test.db" : "bookclub.db"
-};
--- build/routes/utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.rejectNonJSON = function (req, res, next) {
-    if (String(req.headers['content-type']).toLowerCase() !== 'application/json') {
-        return res.status(406).json({ error: "Only application/json accepted" });
-    }
-    next();
-};
